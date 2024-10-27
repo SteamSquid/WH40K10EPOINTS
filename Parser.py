ad23aab9b24f64eb24d13748bb4c83f2cfd1c3a2
josiah <armstong-josiah@proton.me>
2024-10-26 17:21:05 -0700

    most recent update

--- Parser.py
+++ Parser.py
@@ -16,6 +16,9 @@ def main():
     # The name of the XML file to be parsed
     XML_Name="munitionsManual.xml"
 
+    # The array that all the units will be stored in
+    info = []
+
     """
     # Opening the PDF and convert it to an XML
     pdf = pdfquery.PDFQuery(PDF_Name)
@@ -29,13 +32,38 @@ def main():
 
     # Print the things
     # root is base of tree
-    # 1st [] is page 
-    # 2nd [] is page element
-    # 3rd [] 
+    # 1st [] is page                        tag: LTPage
+    # 2nd [] is large elements on page      tag: LTRect, LTImage, LTFigure
+    # 3rd [] is something I don't know yet  tag: LTTextLineHorizontal 
+    # 4th [] the jucy bity we want          tag: LTTextBoxHorizontal 
 
 
-    print(root[1][0][1].tag)
-        
+    
+    
+    page = root[1][0]
+
+    
+    for unit_box in page:
+        unit=[]
+        for text in unit_box:
+            unit.append(text.text)
+        info.append(unit)
+
+    info = [unit for unit in info if unit != []]
+
+    infoTemp = []
+
+    for unitbox in info:
+        unitbox = [element for element in unitbox if unitbox is not None]
+        infoTemp.append(unitbox)
+
+
+    for unitbox in info:
+        unitbox = [element for element in unitbox if unitbox != []]
+        infoTemp.append(unitbox)
+    
+    for element in infoTemp:
+        print(element)
 
 
 if __name__ == "__main__":
